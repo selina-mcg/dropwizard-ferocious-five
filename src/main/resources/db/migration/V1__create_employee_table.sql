73dd01275781952f0d684c47537e588985e0ea2b
thomkainos <thomas.phan@kainos.com>
2024-07-04 10:24:13 +0100

    employee script

---
+++ src/main/resources/db/migration/V1__create_employee_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Employee (
+              id smallint PRIMARY KEY AUTO_INCREMENT,
+              name varchar(255) NOT NULL,
+              salary decimal(11, 2) NOT NULL,
+              bank_acc_num varchar(34) NOT NULL,
+              nin char(9) NOT NULL
+);
+
